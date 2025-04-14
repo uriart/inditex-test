e0d570a106c90f642d1c2455f921e914a4aa7211
uriart <loiro_8@hotmail.com>
2025-04-14 11:59:49 +0200

    [main] First commit

---
+++ src/main/java/com/inditex/demo/InditexApplication.java
@@ -0,0 +1,13 @@
+package com.inditex.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InditexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InditexApplication.class, args);
+	}
+
+}
